16f3fa16349087661dd2d4179b5fe75d26bac12c
Lewis Hammond <lewis@bubblemap.co.uk>
2021-11-02 12:56:38 +0000

    Create Base Agent class
    Created BaseAgent class in AI Shader asmdef

--- Assets/Scripts/AI/Behaviour Tree/BTAgent.cs
+++ Assets/Scripts/AI/Behaviour Tree/BTAgent.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using AI.BehaviourTrees.BaseTypes;
-using AI.BehaviourTrees.TestNodes;
 using UnityEngine;
 
 namespace AI.BehaviourTrees
 {
-    public class Agent : MonoBehaviour
+    public class BTAgent : AI.BaseAgent 
     {
         [SerializeField] private BehaviourTree treePrefab;
         public BehaviourTree RunningTree { get; private set; }
 
         private void Start()
         {
-            //Clone the tree perfab so we have an unique instance that we can run
+            //Clone the tree prefab so we have an unique instance that we can run
             RunningTree = treePrefab.Clone();
         }
 
---
+++ Assets/Scripts/AI/Shared/BaseAgent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace AI
+{
+    /// <summary>
+    /// Base class of agent used for shared functions between both types of AI
+    /// </summary>
+    public class BaseAgent : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Test");
+        }
+    }
+}
