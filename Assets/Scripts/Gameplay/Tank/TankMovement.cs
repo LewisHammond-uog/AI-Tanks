1ad599721fe487140fdd04d8bad5030e760fa8ad
Lewis Hammond <lewis@bubblemap.co.uk>
2021-11-02 14:25:03 +0000

    Add Navmesh Agent to Tank

--- Assets/Scripts/Gameplay/Tank/TankMovement.cs
+++ Assets/Scripts/Gameplay/Tank/TankMovement.cs
@@ -1,40 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 namespace Complete
 {
     public class TankMovement : MonoBehaviour
     {
-        public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
-        public float m_Speed = 12f;                 // How fast the tank moves forward and back.
-        public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
+        public float speed = 12f;                 // How fast the tank moves forward and back.
+        public float turnSpeed = 180f;            // How fast the tank turns in degrees per second.
+        public float acceleration = 0.5f; //How fast the tank accelerates
+        
+        #region Audio
         public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
         public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
         public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
 		public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
-
-        private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
-        private string m_TurnAxisName;              // The name of the input axis for turning.
-        private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-        private float m_MovementInputValue;         // The current value of the movement input.
-        private float m_TurnInputValue;             // The current value of the turn input.
         private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
+        #endregion //Audio
+        
+        private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+
         private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
 
+        private NavMeshAgent movementAgent; //Navmesh Agent used for Navigation
+        
         private void Awake ()
         {
             m_Rigidbody = GetComponent<Rigidbody> ();
+            movementAgent = GetComponent<NavMeshAgent>();
+            if (!movementAgent)
+            {
+                Debug.LogError("No Navmesh Agent for TankMovement. Tank will not move and is likely to cause errors!");
+            }
         }
-
-
+        
         private void OnEnable ()
         {
+            //Stop Navmesh Agent
+            movementAgent.isStopped = true;
+            
+            //Set the navmesh agents values as determined by the tank movement feilds
+            movementAgent.speed = speed;
+            movementAgent.angularSpeed = turnSpeed;
+            movementAgent.acceleration = acceleration;
+            
+            
             // When the tank is turned on, make sure it's not kinematic.
             m_Rigidbody.isKinematic = false;
-
-            // Also reset the input values.
-            m_MovementInputValue = 0f;
-            m_TurnInputValue = 0f;
-
+            
             // We grab all the Particle systems child of that Tank to be able to Stop/Play them on Deactivate/Activate
             // It is needed because we move the Tank when spawning it, and if the Particle System is playing while we do that
             // it "think" it move from (0,0,0) to the spawn point, creating a huge trail of smoke
@@ -43,6 +56,8 @@ namespace Complete
             {
                 m_particleSystems[i].Play();
             }
+            
+            SetDestination(new Vector3(0,0,0),true);
         }
 
 
@@ -61,10 +76,6 @@ namespace Complete
 
         private void Start ()
         {
-            // The axes names are based on player number.
-            m_MovementAxisName = "Vertical" + m_PlayerNumber;
-            m_TurnAxisName = "Horizontal" + m_PlayerNumber;
-
             // Store the original pitch of the audio source.
             m_OriginalPitch = m_MovementAudio.pitch;
         }
@@ -72,14 +83,26 @@ namespace Complete
 
         private void Update ()
         {
-            // Store the value of both input axes.
-            m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
-            m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
+            //EngineAudio ();
+        }
 
-            EngineAudio ();
+        /// <summary>
+        /// Set the destination for the movement, optionally start moving
+        /// </summary>
+        /// <param name="destination">Agent Destination</param>
+        /// <param name="startMovement">If movement should be started niw</param>
+        public void SetDestination(Vector3 destination, bool startMovement = false)
+        {
+            if (movementAgent)
+            {
+                movementAgent.SetDestination(destination);
+                movementAgent.isStopped = false;
+            }
         }
 
 
+        //todo move to own class
+        /*
         private void EngineAudio ()
         {
             // If there is no input (the tank is stationary)...
@@ -105,37 +128,6 @@ namespace Complete
                     m_MovementAudio.Play();
                 }
             }
-        }
-
-
-        private void FixedUpdate ()
-        {
-            // Adjust the rigidbodies position and orientation in FixedUpdate.
-            Move ();
-            Turn ();
-        }
-
-
-        private void Move ()
-        {
-            // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
-            Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
-
-            // Apply this movement to the rigidbody's position.
-            m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-        }
-
-
-        private void Turn ()
-        {
-            // Determine the number of degrees to be turned based on the input, speed and time between frames.
-            float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
-
-            // Make this into a rotation in the y axis.
-            Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
-
-            // Apply this rotation to the rigidbody's rotation.
-            m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
-        }
+        }*/
     }
 }
