429dab59f2bd05cc53772595d2704fb78d6dfc16
Lewis Hammond <lewis@bubblemap.co.uk>
2021-11-02 14:57:29 +0000

    Move Particles to TankParticles
    Removed Particles from TankMovement script to it's own script

--- Assets/Scripts/Gameplay/Tank/TankMovement.cs
+++ Assets/Scripts/Gameplay/Tank/TankMovement.cs
@@ -8,9 +8,7 @@ public class TankMovement : MonoBehaviour
     [SerializeField] private float acceleration = 0.5f; //How fast the tank accelerates
 
     private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-
-    private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
-
+    
     private NavMeshAgent movementAgent; //Navmesh Agent used for Navigation
 
     public float Speed => movementAgent.velocity.magnitude;
@@ -36,31 +34,14 @@ public class TankMovement : MonoBehaviour
         movementAgent.angularSpeed = turnSpeed;
         movementAgent.acceleration = acceleration;
         
-        
         // When the tank is turned on, make sure it's not kinematic.
         m_Rigidbody.isKinematic = false;
-        
-        // We grab all the Particle systems child of that Tank to be able to Stop/Play them on Deactivate/Activate
-        // It is needed because we move the Tank when spawning it, and if the Particle System is playing while we do that
-        // it "think" it move from (0,0,0) to the spawn point, creating a huge trail of smoke
-        m_particleSystems = GetComponentsInChildren<ParticleSystem>();
-        for (int i = 0; i < m_particleSystems.Length; ++i)
-        {
-            m_particleSystems[i].Play();
-        }
     }
-
 
     private void OnDisable ()
     {
         // When the tank is turned off, set it to kinematic so it stops moving.
         m_Rigidbody.isKinematic = true;
-
-        // Stop all particle system so it "reset" it's position to the actual one instead of thinking we moved when spawning
-        for(int i = 0; i < m_particleSystems.Length; ++i)
-        {
-            m_particleSystems[i].Stop();
-        }
     }
     
 
