ec8f7a96276741713837a1839f0f885e551790bd
Lewis Hammond <s1800707@connect.glos.ac.uk>
2021-12-16 13:23:06 +0000

    Flee to Cover
    Added Flee to Cover Actions & Goals

---
+++ Assets/Scripts/Gameplay/Tank/GOAP/TankHealthGOAP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AI.GOAP.Agent;
+using UnityEngine;
+
+namespace Complete.GOAP
+{    
+    //Modification of the TankHealth class for GOAP
+    public class TankHealthGOAP : TankHealth
+    {
+        private GOAPAgent owner;
+
+        //Value below or equal to which we are considered to have low health
+        [SerializeField] private float lowHealthValue = 45f;
+        
+        protected override void Awake()
+        {
+            owner = GetComponent<GOAPAgent>();
+        }
+        
+        private void Update()
+        {
+            //Update agent state for if they have low health
+            owner.ModifyBelief("LowHealth", m_CurrentHealth <= lowHealthValue);
+        }
+    }
+}
