4e80c5a2b110881258a7ca4fffdd38ddc000cbbb
Lewis Hammond <s1800707@connect.glos.ac.uk>
2021-12-16 14:46:22 +0000

    Flee

---
+++ Assets/Scripts/AI/GOAP/Actions/Action_DetermineFleeDirection.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AI.GOAP.Actions
+{
+    public class Action_DetermineFleeDirection : Action
+    {
+        [Header("Reset")] 
+        //Time after which we reset back to NOT having a flee position
+        [SerializeField] private float resetTime = 1.0f;
+        //State that is our effect of having a flee direction - so that we can remove it after 5 seconds
+        [SerializeField] private State hasFleeDirState;
+        
+        protected override bool PrePerform()
+        {
+            //Check that we have a movement component and a enemy to flank
+            if (!Owner.MovementCompoent)
+            {
+                return false;
+            }
+
+            Blackboard.fleeDirection = Vector3.zero;
+            return true;
+        }
+
+        protected override ActionState Perform_Internal()
+        {
+            //Caculate a direction vector to be the average of all of the agents I can see,
+            //then go the other way
+            List<BaseAgent> visibleAgents = Owner.VisionKnowledgeComponent.GetVisibleAgents().ToList();
+            if (visibleAgents.Count == 0)
+            {
+                return ActionState.Fail;
+            }
+        
+            Vector3 sumVector = Vector3.zero;
+            foreach (BaseAgent agent in visibleAgents)
+            {
+                //Add the direction
+                sumVector += (agent.transform.position - Owner.transform.position).normalized;
+            }
+            //Get the average and normalize for a direction
+            Vector3 agentAvgDirection = (sumVector / visibleAgents.Count).normalized;
+        
+            //Go the other direction!
+            Blackboard.fleeDirection = -agentAvgDirection;
+            
+            //Add Belief to agents
+            Owner.AddBelief(hasFleeDirState);
+            
+            return ActionState.Success;
+        }
+
+        protected override bool PostPerform()
+        {
+            //Expire flee dir after time
+            StartCoroutine(Owner.RemoveBeliefAfterTime(hasFleeDirState.key, resetTime));
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/GOAP/Actions/Action_Flee.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace AI.GOAP.Actions
+{
+    public class Action_Flee : Action
+    {
+        
+        [Tooltip("Distance to project the flee direction when attemping to project it far away")]
+        [SerializeField] private float farProjectDistance = 10f;
+        
+        [Tooltip("Distance to project the flee direction when attemping to project it close")]
+        [SerializeField] private float closeProjectDistance = 2f;
+        
+        //Have we already set a position on this node runthrough?
+        private bool positionSet;
+        
+        protected override bool PrePerform()
+        {
+            //Check that we have a valid direction
+            if (Blackboard?.fleeDirection == Vector3.zero)
+            {
+                return false;
+            }
+
+            positionSet = false;
+            
+            return true;
+        }
+
+        protected override ActionState Perform_Internal()
+        {
+            if (positionSet == false)
+            {
+                //Project the flee direction, see if we can go far, then less far
+                if (Owner.MovementCompoent.SetDestination(Blackboard.fleeDirection * farProjectDistance, true))
+                {
+                    return ActionState.Running;
+                }
+                else if (Owner.MovementCompoent.SetDestination(Blackboard.fleeDirection * closeProjectDistance, true))
+                {
+                    return ActionState.Running;
+                }
+                else
+                {
+                    //We cannot project less than our shortest flee distance we therefore cannot run away!
+                    return ActionState.Fail;
+                }
+            }
+            
+            //Check if we are at destination
+            return Owner.MovementCompoent.IsAtDestination() ? ActionState.Success : ActionState.Running;
+        }
+
+        protected override bool PostPerform()
+        {
+            return true;
+        }
+    }
+}
