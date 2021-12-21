60e7a4d9dc705b514db5a11822805f96af741487
Lewis Hammond <s1800707@connect.glos.ac.uk>
2021-12-21 23:10:28 +0000

    GOAP - Abort Wander for Team
    Abort the wander state if our team mate has seen an enemy

--- Assets/Scripts/AI/GOAP/Actions/Action_Wander.cs
+++ Assets/Scripts/AI/GOAP/Actions/Action_Wander.cs
@@ -7,6 +7,9 @@ namespace AI.GOAP.Actions
         //State that we have a wander position, so that we can remove it after we have finished wandering
         [SerializeField] private State hasWanderPosState;
 
+        //State for when a teammate has seen an enemy - so that we can break out of our wander
+        [SerializeField] private State teamSeenEnemyState;
+        
         //If we have set the player to move to random pos
         private bool hasSetPos;
         
@@ -19,6 +22,12 @@ namespace AI.GOAP.Actions
 
         protected override ActionState Perform_Internal()
         {
+            //Interupt if our teammate(s) have seen an enemy
+            if (Owner.HasBelief(teamSeenEnemyState))
+            {
+                return ActionState.Fail;
+            }
+            
             //Set Destination if we don't already have it set
             if (Owner.MovementCompoent.Destination != AgentBlackboard.randomWanderPos)
             {
